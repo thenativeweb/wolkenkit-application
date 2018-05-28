78c69a1414d1927e686b1ca52e1f9c2d926fd3f7
Golo Roden <golo.roden@thenativeweb.io>
2018-05-28 09:45:43 +0200

    Release 2.0.1.

--- dist/loadWriteModel/commandBuilder/authorize.js
+++ dist/loadWriteModel/commandBuilder/authorize.js
@@ -1,7 +1,15 @@
 'use strict';
 
+var humanizeString = require('humanize-string');
+
 var authorize = function authorize(aggregateInstance, command) {
   try {
+    if (!aggregateInstance.exists()) {
+      var aggregateName = humanizeString(command.aggregate.name);
+
+      throw new Error(aggregateName + ' does not exist.');
+    }
+
     aggregateInstance.authorize(command.data);
   } catch (ex) {
     return command.reject(ex.message);
--- dist/loadWriteModel/commandBuilder/transferOwnership.js
+++ dist/loadWriteModel/commandBuilder/transferOwnership.js
@@ -1,7 +1,15 @@
 'use strict';
 
+var humanizeString = require('humanize-string');
+
 var transferOwnership = function transferOwnership(aggregateInstance, command) {
   try {
+    if (!aggregateInstance.exists()) {
+      var aggregateName = humanizeString(command.aggregate.name);
+
+      throw new Error(aggregateName + ' does not exist.');
+    }
+
     aggregateInstance.transferOwnership(command.data);
   } catch (ex) {
     return command.reject(ex.message);
